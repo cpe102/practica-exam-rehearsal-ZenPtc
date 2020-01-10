7881726754ad3404223f6f1b0ae822e2f2f58482
ZenPtc <maku4fb@gmail.com>
2020-01-10 15:16:42 +0700

    test

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# NAME:
-# STUDENT ID:
-# มีอะไรอยากจะบอกอาจารย์ไหม:
+# NAME:Naruporn Powthongchin
+# STUDENT ID:620610794
+# มีอะไรอยากจะบอกอาจารย์ไหม:ไม่มีค่า
--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-
+#include <cmath>
+using namespace std;
 //Write sumSqrt() here.
-
+float sumSqrt(int N){
+    if(N > 0){
+        float sum = 0;
+        int i = 1;
+        while(i <= N){
+            sum += 1/sqrt(i);
+            i++;
+        }
+        return sum;
+    }else{
+        return 0;
+    }
+}
 
 int main()
 {
@@ -12,7 +25,9 @@ int main()
     double e = sumSqrt(1);
     double f = sumSqrt(0);
     double g = sumSqrt(-1);
+    double h = sumSqrt(-5);
+    double i = sumSqrt(3);
     
     cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
-
+    cout << h << endl << i;
 }
